f2c6c8c1082339c0379cbd2513eeef78cea82251
Magarreiro De Deus Tiago Miguel <tmmagarreiro@emea.msad.sopra>
2015-11-30 12:19:37 +0100

    getterMethods in worker and career table creation

--- models/worker.js
+++ models/worker.js
@@ -1,67 +1,89 @@
 "use strict";
 
 module.exports = function(sequelize, DataTypes) {
-	var Worker = sequelize.define('Worker', {
-		id: {
-			type:DataTypes.INTEGER, 
-			primaryKey:true,
-			comment: 'worker id'
-		},
-		name: {
-			type:DataTypes.STRING(100), 
-			allowNull:false,
-			comment: 'worker name'
-		},
-		surname: {
-			type:DataTypes.STRING(150), 
-			allowNull:false,
-			comment: 'worker surname'
-		},
-		birthday: {
-			type:DataTypes.DATEONLY, 
-			allowNull:false,
-			comment: 'worker birthday',
-			get: function() {
-				var date = new Date(this.getDataValue('birthday')); 
-				return date.getTime();
-			}
-		},
-		email: {
-			type:DataTypes.STRING(150), 
-			allowNull:false,
-			isEmail: true,
-			comment: 'worker email'
-		},
-		document_number: {
-			type:DataTypes.STRING(20), 
-			allowNull:false,
-			comment: 'worker identification document number'
-		},
-		startup_date: {
-			type:DataTypes.DATEONLY,
-			comment: 'worker startup date in the company'
-		},
-		salary: {
-			type:DataTypes.DECIMAL(10,2),
-			comment: 'worker current gross salary by year'
-		}
-	},{
-		underscored: true,
-		timestamps: true,
-		tableName: 'worker',
-		comment: 'worker data main table',
-		paranoid: true,
-		classMethods: {
-			associate: function(models) {
-				Worker.belongsTo(models.TypeDocument, {
-					as: 'document_type',
-					comment: 'worker identification document type',
-					foreignKey: {
-            			allowNull: false
-          			}
-				});
-			}
-		}
-	});
-	return Worker;
+    var Worker = sequelize.define('Worker', {
+        id: {
+            type:DataTypes.INTEGER, 
+            primaryKey:true,
+            comment: 'worker id'
+        },
+        name: {
+            type:DataTypes.STRING(100), 
+            allowNull:false,
+            comment: 'worker name'
+        },
+        surname: {
+            type:DataTypes.STRING(150), 
+            allowNull:false,
+            comment: 'worker surname'
+        },
+        birthday: {
+            type:DataTypes.DATEONLY, 
+            allowNull:false,
+            comment: 'worker birthday',
+            get: function() {
+                var date = new Date(this.getDataValue('birthday')); 
+                return date.getTime();
+            }
+        },
+        email: {
+            type:DataTypes.STRING(150), 
+            allowNull:false,
+            isEmail: true,
+            comment: 'worker email'
+        },
+        document_number: {
+            type:DataTypes.STRING(20), 
+            allowNull:false,
+            comment: 'worker identification document number'
+        },
+        startup_date: {
+            type:DataTypes.DATEONLY,
+            comment: 'worker startup date in the company',
+            get: function() {
+                var date = new Date(this.getDataValue('startup_date')); 
+                return date.getTime();
+            }
+        },
+        salary: {
+            type:DataTypes.DECIMAL(10,2),
+            comment: 'worker current gross salary by year'
+        }
+    },{
+        underscored: true,
+        timestamps: true,
+        tableName: 'worker',
+        comment: 'worker data main table',
+        paranoid: true,
+        classMethods: {
+            associate: function(models) {
+                Worker.belongsTo(models.TypeDocument, {
+                    as: 'document_type',
+                    comment: 'worker identification document type',
+                    foreignKey: {
+                        allowNull: false
+                    }
+                });
+            }
+        },
+        getterMethods: {
+            created_at: function()  { 
+                var date = new Date(this.getDataValue('created_at')); 
+                return date.getTime();
+            },
+            updated_at: function()  { 
+                var date = new Date(this.getDataValue('created_at')); 
+                return date.getTime(); 
+            },
+            deleted_at: function()  { 
+                if (this.getDataValue('deleted_at')) {
+                    var date = new Date(this.getDataValue('deleted_at')); 
+                    return date.getTime(); 
+                } else {
+                    return null;
+                }
+            }
+        }
+    });
+    return Worker;
 };
--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,41 @@
 function Util () {}
 
-Util.formatDate = function(value){
-	var date = new Date(value); 
-	var month = date.getMonth();
-	var day = date.getDate();
-	var year = date.getFullYear();
-	return day+'/'+month+'/'+year;
+Util.formatDate = function( col ){
+    var value = this.getDataValue(col);
+    if (value) {
+        var date = new Date(value); 
+        var month = date.getMonth();
+        var day = date.getDate();
+        var year = date.getFullYear();
+        return day+'/'+month+'/'+year;
+    } else {
+        return null;
+    }
+};
+
+Util.getTimestamp = function( col )  { 
+    var value = this.getDataValue(col);
+    if (value) {
+        var date = new Date(value); 
+        return date.getTime(); 
+    } else {
+        return null;
+    }
+};
+
+Util.remove_empty = function ( target ) {
+  Object.keys( target ).map( function ( key ) {
+    if ( target[ key ] instanceof Object ) {
+      if ( ! Object.keys( target[ key ] ).length && typeof target[ key ].getMonth !== 'function') {
+        delete target[ key ];
+      }
+      else {
+        remove_empty( target[ key ] );
+      }
+    }
+    else if ( target[ key ] === null ) {
+      delete target[ key ];
+    }
+  } );
+  return target;
 };
